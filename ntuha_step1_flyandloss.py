2a5909e5849356ba942429846d7f44dd9636d312
wicebing <32655081+wicebing@users.noreply.github.com>
2024-11-16 12:53:01 +0800

    aa
    aa

---
+++ ntuha_step1_flyandloss.py
@@ -0,0 +1,195 @@
+import pandas as pd
+import numpy as np
+from PIL import Image
+import datetime,os,math, pytz, json, pickle
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import matplotlib.colors as mcolors
+from matplotlib.colors import to_rgb, to_rgba
+
+from pykalman import KalmanFilter
+from scipy.signal import savgol_filter
+
+
+import utils
+
+databank_filepath = "./guider20240808/databank/positions"
+os.makedirs(databank_filepath,exist_ok=True)
+
+local_timezone = pytz.timezone('Asia/Taipei')  
+
+select_beacons =['N002', 'N003', 'N004', 'N005', 'N006', 'N007', 'N008', 'N017', 'N029']
+beacon_ids = select_beacons #utils.get_beacons()
+print('=== load beacons ids ===')
+
+x_min=302491
+x_max=302516
+y_min=2770397
+y_max=2770422
+scale = 45
+grid_size = 45
+txyzPds = {}
+txyzPds_origin = {}
+txyzPds_smooth = {}
+txyzOutlier = {}
+
+def filter_single(df, time_col='positionTime'):
+    dfc = df.copy().sort_values(by=time_col)
+
+    # Calculate differences in position and time (in seconds)
+    dfc['x_diff'] = dfc['x'].diff()
+    dfc['y_diff'] = dfc['y'].diff()
+    dfc['time_diff'] = dfc[time_col].diff().dt.total_seconds()
+    dfc['position_diff'] = (dfc['x_diff']**2 + dfc['y_diff']**2)**0.5
+    
+    dfc['group'] = dfc['position_diff'] > 2.5
+    # Handle cases with large missing time gaps
+    dfc['skip'] = 0
+    
+    dfc.loc[(dfc['time_diff']>10) & (dfc['position_diff']>2), 'skip'] +=1
+    dfc.loc[(dfc['time_diff']>10) & (dfc['position_diff']>2), 'group'] = True
+    dfc['skip'] = dfc['skip'].cumsum()
+    dfc['group'] = dfc['group'].cumsum()
+
+    return dfc
+
+for beacon in beacon_ids:
+    recordName= f'{beacon}.pkl'
+    pickle_filepath = os.path.join(databank_filepath,recordName)
+        
+    if os.path.isfile(pickle_filepath):
+        print(f'=== {beacon}.pkl exist, loading ===')
+
+        txyzPd_origin = pd.read_pickle(pickle_filepath)
+        pd_pz = pd.json_normalize(txyzPd_origin['position'])
+        pd_time = pd.to_datetime(txyzPd_origin['positionTime'],format='mixed').dt.tz_convert(local_timezone)
+        df = pd.concat([pd_time,pd_pz],axis=1)
+        
+        df['x'] = df['x']-x_min
+        df['y'] = df['y']-y_min
+        
+        txyzPds_origin[beacon]=df
+        
+        aao = df.dropna().copy()
+        out_boundary = (aao['x']<0)|((aao['x']>25))|(aao['y']<0)|((aao['y']>25))
+        txyzOutlier[beacon] = {'origin':len(aao),'outlier':0, 'out_boundary':out_boundary.sum()}
+        print(f'out_boundary {out_boundary.sum()}')
+        aao = aao.loc[~out_boundary]
+        
+        outliers = 1
+        while(outliers>0):
+            aa = filter_single(aao)
+            skip_count = aa.value_counts('skip')
+            aa['skip_num'] = skip_count[aa.skip].values
+            group_count = aa.value_counts('group')
+            aa['group_num'] = group_count[aa.group].values
+            drop = (aa['skip_num']<=1)
+            aao = aao.loc[~drop]
+            print(len(aa)-len(aao),len(aa),len(aao))
+            outliers = len(aa)-len(aao)
+            txyzOutlier[beacon]['outlier'] += outliers
+        
+        threshold = 2
+        outliers = 1
+        while(outliers>0):
+            aa = filter_single(aao)
+            group_lapse = aa.groupby('group')['time_diff'].sum()
+            aa['group_lapse'] = group_lapse[aa.group].values
+            skip_count = aa.value_counts('skip')
+            aa['skip_num'] = skip_count[aa.skip].values
+            group_count = aa.value_counts('group')
+            aa['group_num'] = group_count[aa.group].values
+            drop = (aa['group_num']<=threshold)
+            aao = aao.loc[~drop]
+            print(threshold,drop.sum(),len(aa)-len(aao),len(aa),len(aao))
+            outliers = len(aa)-len(aao)
+            txyzOutlier[beacon]['outlier'] += outliers
+
+
+        outliers_group = 1
+        while_loop = 0
+        
+        while(outliers_group>0):
+            aa = filter_single(aao)
+            groupInTime= aa.groupby('group')['skip'].value_counts()
+            groupInTime = groupInTime.reset_index().set_index('group')
+            group_lastxy = aa.groupby('group')[['x','y']].last()
+            group_firstxy = aa.groupby('group')[['x','y']].first()
+            group_diff = aa.groupby('group')[['time_diff','position_diff']].first()
+            group_x = aa.groupby('group')['x'].mean()
+            group_y = aa.groupby('group')['y'].mean()
+            group_count = aa.value_counts('group')
+            group_lapse = aa.groupby('group')['time_diff'].sum()
+    
+            drop_group = []
+            for gp in range(len(groupInTime)):
+                now_x = group_x[gp]
+                now_y = group_y[gp]
+                if now_x < 1 or now_x >24:
+                    drop_group.append(gp)
+                if now_y < 1 or now_y >26:
+                    drop_group.append(gp)
+                    
+                if gp==0: continue
+            
+                skip_now = groupInTime.loc[gp]['skip']
+                skip_m1 = groupInTime.loc[gp-1]['skip']
+                sss = group_diff.loc[gp]
+                xm1,ym1 = group_lastxy.loc[gp-1]
+                x1,y1 = group_firstxy.loc[gp] 
+                
+                if skip_now > skip_m1: continue        
+                if sss['position_diff'] < sss['time_diff']: 
+                    if xm1<=9.5 and (x1>10.5 or now_x>11):
+                        if group_count[gp]<60 or group_lapse[gp] <60:
+                            drop_group.append(gp)
+                    continue        
+                if xm1<=9.5 and (x1>10.5 or now_x>11):
+                    if group_count[gp]<60 or group_lapse[gp] <60:
+                        drop_group.append(gp)
+                
+                now_xm1 = group_x[gp-1]
+                if x1<=9.5 and (xm1>10.5):
+                    if group_count[gp-1]<60 or group_lapse[gp-1] <60:
+                        drop_group.append(gp-1)
+                    
+            drop_group=list(set(drop_group))
+            outliers_group = len(drop_group)
+            
+            if outliers_group >0:
+                drop_idx_ = []
+                for dpg in drop_group:
+                    drop = aa['group']==dpg
+                    drop_idx_.append(drop)
+
+                drop_idx = pd.concat(drop_idx_,axis=1)
+                dropAll = drop_idx.sum(axis=1).astype(bool)
+                txyzOutlier[beacon]['outlier'] += dropAll.sum()
+                print(f'drop {while_loop} {dropAll.sum()}')
+                aao = aao.loc[~dropAll]
+                while_loop += 1            
+        
+        txyzPds[beacon]=aao
+        
+        print(' == doing the smooth ==')
+        aa = filter_single(aao)
+        for sk in range(aa.skip.max()):
+            print(f' == smooth {beacon} {sk} ==')
+            temp = aa[aa['skip']==sk].copy()
+            for axs in ['x','y']:
+                initial_state_mean = temp.iloc[0][axs]
+                kf = KalmanFilter(initial_state_mean=initial_state_mean)
+                smoothed_k = kf.smooth(temp[axs])[0]
+                
+                aa.loc[temp.index,[axs]] = smoothed_k
+                
+        txyzPds_smooth[beacon]=aa
+        
+
+with open("./guider20240808/databank/pkl/origin.pkl", 'wb') as f:
+    pickle.dump(txyzPds_origin, f)    
+with open("./guider20240808/databank/pkl/filter01.pkl", 'wb') as f:
+    pickle.dump(txyzPds, f)
+with open("./guider20240808/databank/pkl/KalmanSmooth01.pkl", 'wb') as f:
+    pickle.dump(txyzPds_smooth, f)
+pd.DataFrame(txyzOutlier).to_excel('./output/outliers.xlsx')
---
+++ ntuha_step2_plot_position.py
@@ -0,0 +1,178 @@
+import pandas as pd
+import numpy as np
+from PIL import Image
+import datetime,os,math, pytz, json, pickle
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import matplotlib.colors as mcolors
+from matplotlib.colors import to_rgb, to_rgba
+
+import utils
+
+databank_filepath = "./guider20240808/databank/positions"
+os.makedirs(databank_filepath,exist_ok=True)
+
+local_timezone = pytz.timezone('Asia/Taipei') 
+ 
+select_beacons =['N002', 'N003', 'N004', 'N005', 'N006', 'N007', 'N008', 'N017', 'N029']
+beacon_ids = select_beacons #utils.get_beacons()
+print('=== load beacons ids ===')
+
+x_min=302491
+x_max=302516
+y_min=2770397
+y_max=2770422
+scale = 45
+grid_size = 45
+# txyzPds = {}
+
+# for beacon in beacon_ids:
+#     recordName= f'{beacon}.pkl'
+#     pickle_filepath = os.path.join(databank_filepath,recordName)
+    
+#     if os.path.isfile(pickle_filepath):
+#         print(f'=== {beacon}.pkl exist, loading ===')
+
+#         txyzPd_origin = pd.read_pickle(pickle_filepath)
+#         pd_pz = pd.json_normalize(txyzPd_origin['position'])
+#         pd_time = pd.to_datetime(txyzPd_origin['positionTime'],format='mixed').dt.tz_convert(local_timezone)
+#         df = pd.concat([pd_time,pd_pz],axis=1)
+        
+#         df['x'] = df['x']-x_min
+#         df['y'] = df['y']-y_min
+        
+#         txyzPds[beacon]=df
+
+def plot_trajectory(dfs, evt_x, evt_y, evt_what, pic_name='evtTimePoint',grid=False):
+    """Plots the trajectory of points from a DataFrame.
+
+    Args:
+        df: Pandas DataFrame with 'positionTime' (datetime) and 'position' (dict).
+    """
+    x_min=302491 - 302491
+    x_max=302516 - 302491
+    y_min=2770397 - 2770397
+    y_max=2770422 - 2770397
+    scale = 45
+    grid_size = 45
+    
+    fig, ax = plt.subplots(figsize=(10, 10))  # adjust figsize for better view
+    # Load the image
+    img = Image.open('./guider20240808/databank/ED_Area.png')
+    img_array = np.array(img)
+    img_array = np.flipud(img_array)     
+    ax.imshow(img_array)
+    
+    colors = {'N002':'blue',
+              'N003':'violet',
+              'N004':'limegreen',
+              'N005':'darkorange',
+              'N006':'tomato',
+              'N007':'royalblue',
+              'N008':'peru',
+              'N017':'salmon',
+              'N029':'peru',}
+    
+        
+    for k,d in dfs.items():
+        df = d.copy()
+        # Extract x and y coordinates; handle potential errors.
+        x = scale*(df['x']-x_min)
+        y = scale*(df['y']-y_min)
+        
+        df['time_diff'] = df['positionTime'].diff().dt.total_seconds()
+        
+        times = df['positionTime'].values
+        
+        # Calculate alpha values for transparency (optional)
+        alpha_values = np.linspace(0.0, 0.75, len(x)) # Adjust transparency as needed
+        r, g, b = to_rgb(colors[k])
+        clr = [(r, g, b, alpha) for alpha in np.floor(alpha_values*10)/10]
+        
+        # Plot points with transparency
+        ax.scatter(x, y, c = clr, alpha=0.5, s = 15)
+
+        # Add a line connecting the points
+        consecutive_indices = np.where(df['time_diff'].values > 10)
+        if consecutive_indices[0].size > 1:
+            cons_i = 0
+            for i, idx in enumerate(consecutive_indices[0]):
+                x_consecutive = x[cons_i:idx]
+                y_consecutive = y[cons_i:idx]
+                ax.plot(x_consecutive, y_consecutive, color=colors[k], alpha = 0.1, linestyle = '-')
+                cons_i=idx
+            if i == consecutive_indices[0].size-1:
+                x_consecutive = x[cons_i:]
+                y_consecutive = y[cons_i:]
+                ax.plot(x_consecutive, y_consecutive, color=colors[k], alpha = 0.1, linestyle = '-')
+        else:
+            ax.plot(x, y, color=colors[k], alpha = 0.1, linestyle = '-') 
+    
+    # plot event point
+    if evt_what == '轉重症':
+        ax.scatter(scale*(evt_x-x_min),scale*(evt_y-y_min), marker='P', s =300, c='black')
+    else:
+        ax.scatter(scale*(evt_x-x_min),scale*(evt_y-y_min), marker='P', s =300, c='black')
+        ax.scatter(scale*(evt_x-x_min),scale*(evt_y-y_min), marker='P', s =88, c='lightyellow')
+
+    # Set plot limits
+    major_ticks = np.arange(0, 1125, grid_size)
+    ax.grid(which='major', alpha=0.5, linestyle='--')
+    ax.set_xticks(major_ticks)
+    ax.set_yticks(major_ticks)
+    if(not grid):
+        plt.xticks([])
+        plt.yticks([])
+    
+    ax.set_xlim(0,scale*(x_max-x_min))
+    ax.set_ylim(0,scale*(y_max-y_min))
+    
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_title(f'Position Trajectory_{pic_name}')
+
+    # plt.axis('off')
+    plt.grid(True)
+    plt.savefig(fname=f'./output/pic/{pic_name}.png')
+    print(f' === complete {pic_name} image === ')
+
+# Draw Trajectory 
+def Trajectory_plot(events, drawPds,hours=1,flag='origin',grid=False):
+    for i, evt in events.iterrows():
+        print(f' == work on {i} event == ')
+        positionTime = evt['positionTime']
+        evt_x = evt['X']
+        evt_y = evt['Y']
+        evt_what = evt['事件分類']
+        發生地點 = evt['發生地點']
+        endtime = positionTime-datetime.timedelta(minutes=30)
+        startTime = endtime-datetime.timedelta(hours=hours)
+        
+        # if i != 30:
+        #     continue
+        dfs = {}
+        for beacon in select_beacons:
+            df = drawPds[beacon].loc[(drawPds[beacon]['positionTime'] >= startTime) & (drawPds[beacon]['positionTime'] <= endtime)]    
+            if len(df) > 0:
+                dfs[beacon]=df
+        
+        plot_trajectory(dfs, evt_x-x_min, evt_y-y_min, evt_what, pic_name=f'{i+1}_{發生地點}_{positionTime.hour}_{hours}hour_{flag}', grid=grid)
+ 
+# Load the event timePoint
+events = pd.read_excel("./guider20240808/databank/events.xlsx")
+events['日期'] = events['日期'].astype(str)
+events['時間'] = events['時間'].astype(str)
+events['positionTime'] = pd.to_datetime(events['日期'] + ' ' + events['時間'], format='%Y-%m-%d %H%M', errors='coerce').dt.tz_localize(local_timezone)
+events = events[['positionTime','發生地點','事件分類', 'X', 'Y']]
+
+# Load the beacon positionTime
+with open("./guider20240808/databank/pkl/origin.pkl", 'rb') as f:
+    txyzPds_origin = pickle.load(f)
+with open("./guider20240808/databank/pkl/filter01.pkl", 'rb') as f:
+    txyzPds = pickle.load(f)   
+with open("./guider20240808/databank/pkl/KalmanSmooth01.pkl", 'rb') as f:
+    txyzPds_smooth = pickle.load(f)
+
+Trajectory_plot(events, txyzPds_smooth,3,'smooth_0',grid=False)       
+Trajectory_plot(events, txyzPds,3,'filter_0',grid=False)       
+Trajectory_plot(events, txyzPds_origin,3,'',grid=False)      
