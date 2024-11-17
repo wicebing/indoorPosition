90d7c2e1375cef6b00c42d6bc630c082dc4504c6
wicebing <32655081+wicebing@users.noreply.github.com>
2024-11-17 23:15:56 +0800

    aa
    aa

--- ntuha_step5_count_nurse_loading.py
+++ ntuha_step5_count_nurse_loading.py
@@ -87,34 +87,57 @@ loadings = []
 for k, v in aa.items():
     res = v.groupby(['id_hours', 'skip']).agg({'time_diff': 'sum', 'position_diff': 'sum'})
     load = res.reset_index().groupby(['id_hours']).agg({'time_diff': 'sum', 'position_diff': 'sum'})
+    load['mps'] = load['position_diff']/load['time_diff']
+    load = load.rename(columns={'time_diff': f'time_diff_{k}', 
+                                'position_diff': f'position_diff_{k}',
+                                'mps': f'mps_{k}',})
     loadings.append(load.reset_index())
 
 load_all = reduce(lambda x, y: x.merge(y, how='outer', on='id_hours'), loadings)
+# Sum time_diff columns (after renaming)
+time_diff_cols = [col for col in load_all.columns if col.startswith('time_diff_')]
+load_all['time_diff_all'] = load_all[time_diff_cols].sum(axis=1)
+position_diff_cols = [col for col in load_all.columns if col.startswith('position_diff_')]
+load_all['position_diff_all'] = load_all[position_diff_cols].sum(axis=1)
+load_all['mps_all'] = load_all['position_diff_all']/load_all['time_diff_all']
+
+load_all['event'] = 0
+load_all = load_all.set_index('id_hours')
+
+mps_cols = [col for col in load_all.columns if col.startswith('mps_')]
+plot_data = load_all[mps_cols].copy()
 
 
+# Load the event timePoint
+events = pd.read_excel("./guider20240808/databank/events.xlsx")
+events['日期'] = events['日期'].astype(str)
+events['時間'] = events['時間'].astype(str)
+events['positionTime'] = pd.to_datetime(events['日期'] + ' ' + events['時間'], format='%Y-%m-%d %H%M', errors='coerce').dt.tz_localize(local_timezone)
+events = events[['positionTime','發生地點','事件分類', 'X', 'Y']]
 
-def export_lossTick_to_excel(lossTick, output_filename="./output/lossTick_report.xlsx"):
-    """Exports lossTick data to an Excel file with multiple sheets.
-
-    Args:
-        lossTick (dict): A dictionary where keys are names of sheets (e.g., device IDs or 'all')
-                         and values are dictionaries containing 'lossTick', 'byWDH', and 'lossTickPercent' DataFrames.
-        output_filename (str): The name of the output Excel file.
-    """
-
-    with pd.ExcelWriter(output_filename) as writer:
-        for sheet_name, data_dict in lossTick.items():
-            for data_label, df in data_dict.items():
-
-                #To handle multiindex dataframe to be exportable
-                df = df.copy()  # Create a copy to avoid modifying the original
-                if isinstance(df.columns, pd.MultiIndex):
-                    df.columns = ['_'.join(map(str, col)).strip('_') for col in df.columns.values]
-                if isinstance(df.index, pd.MultiIndex):
-                    df.index = ['_'.join(map(str, col)).strip('_') for col in df.index.values]
-
-                df.to_excel(writer, sheet_name=f"{sheet_name}_{data_label}")  # Include the label in the sheet name
+for i, evt in events.iterrows():
+    print(f' == work on {i} event == ')
+    positionTime = evt['positionTime']
+    evt_x = evt['X']
+    evt_y = evt['Y']
+    evt_what = evt['事件分類']
+    發生地點 = evt['發生地點']
+    endtime = positionTime
+    startTime = endtime-datetime.timedelta(hours=3)
+    
+    # load_all.loc[endtime,'event'] = 1 if evt_what=='轉重症' else 2
 
 
-# Example usage:
-# export_lossTick_to_excel(lossTick)
+# consecutive_indices = np.where(load_all['event'].values==1)
+
+# if consecutive_indices[0].size > 1:
+#     cons_i = 0
+#     for i, idx in enumerate(consecutive_indices[0]):
+    fig, ax = plt.subplots(figsize=(20, 10))  # adjust figsize for better view
+    
+    x_consecutive = plot_data.loc[startTime:endtime]
+
+    ax = x_consecutive.plot(figsize=(30,10),ylim=(0,0.8))
+    plt.savefig(fname=f'./output/loading/{i}_{evt_what}.png')
+
+ 
